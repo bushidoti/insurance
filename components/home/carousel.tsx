9473d576fddba3015d3577ac8a65eabd27a9271a
Sajjad <titanxl79@gmail.com>
2024-04-28 18:01:06 +0330

    change style and details

--- components/home/carousel.tsx
+++ components/home/carousel.tsx
@@ -1,4 +1,5 @@
 import { Button, Carousel } from "flowbite-react";
+import Image from "next/image";
 
 
 const CarouselHome = () => {
@@ -9,77 +10,50 @@ const CarouselHome = () => {
           <div className="lg:max-w-xl xl:shrink-0">
             <div>
               <h2 className="text-3xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white sm:text-5xl">
-                Making earth cooler for people, nature, climate
+                انتخاب امن برای حفاظت از آینده شما
               </h2>
               <p className="mt-5 text-base font-normal text-gray-500 dark:text-gray-400 sm:text-xl md:max-w-3xl">
-                Protecting the planet and its natural resources for future
-                generations, by reducing pollution, promoting sustainability,
-                and conserving energy and resources.
+                بیمه، روشی برای محافظت از خود در برابر خسارت‌های مالی است.
+                بیمه‌گذار متعهد می‌شود خسارت‌ها را در صورت وقوع رویداد خاصی جبران کند.
+                بیمه‌گیرنده نیز حق بیمه را پرداخت می‌کند. این روش، ریسک‌های مالی را کاهش می‌دهد.
               </p>
             </div>
             <div className="mt-8 flex flex-col gap-4 sm:flex-row">
-              <Button size="lg">Donate now</Button>
-              <Button size="lg" color="gray" className="[&>span]:items-center" outline>
+              <Button size="lg" color="gray" className="[&>span]:items-center  hover:!bg-green-500 hover:!text-white" outline>
                 <svg
                   aria-hidden="true"
-                  className="-ml-1 mr-2 h-5 w-5"
+                  className="-mr-1 ml-2 h-5 w-5"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                 >
                   <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
                 </svg>
-                Learn more
+                بیشتر بدانید
               </Button>
-            </div>
-            <div className="mt-4 dark:border-gray-700 sm:mt-8 sm:border-t sm:border-gray-100 sm:pt-8">
-              <p className="hidden text-base font-medium text-gray-500 sm:block">
-                Partners and backers:
-              </p>
-              <div className="mt-3 flex max-w-md items-center">
-                <img
-                  className="mr-4 h-8 w-auto md:h-12"
-                  src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/customers/greenpeace.svg"
-                  alt=""
-                />
-                <img
-                  className="mr-8 h-8 w-auto md:h-12"
-                  src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/customers/united-nations.svg"
-                  alt=""
-                />
-                <img
-                  className="mr-8 h-8 w-auto dark:invert md:h-12"
-                  src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/customers/wwf.svg"
-                  alt=""
-                />
-                <img
-                  className="h-8 w-auto md:h-12"
-                  src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/customers/oxfam.svg"
-                  alt=""
-                />
-              </div>
             </div>
           </div>
           <Carousel dir='ltr' className="h-64 md:h-96">
-              <img
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/ngo-carousel/image-1.jpg"
+              <Image
+                width={600}
+                height={500}
+                src="/carousel2.jpeg"
                 className="rounded-lg"
-                alt=""
+                alt="carousel2"
               />
-              <img
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/ngo-carousel/image-2.jpg"
+              <Image
+                width={600}
+                height={500}
+                src="/carousel3.jpeg"
                 className="rounded-lg"
-                alt=""
+                alt="carousel3"
               />
-              <img
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/ngo-carousel/image-3.jpg"
+              <Image
+                width={600}
+                height={500}
+                src="/carousel4.jpg"
                 className="rounded-lg"
-                alt=""
-              />
-              <img
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/ngo-carousel/image-4.jpg"
-                className="rounded-lg"
-                alt=""
+                alt="carousel4"
               />
           </Carousel>
         </div>
